471ce24629c2abe8c4fa4e758c320fc2768b13e9
Federico Marra <marrafederico@proton.me>
2025-08-07 15:56:03 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -25,7 +25,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowAngularApp",
         policy =>
         {
-            policy.WithOrigins("http://localhost:4200") 
+            policy.WithOrigins("https://friendstuff.vercel.app") 
                 .AllowAnyHeader()
                 .AllowCredentials()
                 .AllowAnyMethod();
